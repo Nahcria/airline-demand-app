2e2f59b0383596a65a3d0efe5032e54a2d2accf9
Nahcria <riyajoshiseven77@gmail.com>
2025-07-08 21:59:12 +0530

    Update Flask host and port for Render

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, jsonify
 import requests
 from bs4 import BeautifulSoup
 import random
+import os
 
 app = Flask(__name__)
 
@@ -27,4 +28,5 @@ def api_data():
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
